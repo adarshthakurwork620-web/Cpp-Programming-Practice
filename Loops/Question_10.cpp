c46bc28617085ac63e13eaf477b5b4c5126957c7
Adarsh22 <adarshthakurwork620@gmail.com>
2026-04-17 08:54:06 +0530

    Add files via upload

---
+++ Loops/Question_10.cpp
@@ -0,0 +1,17 @@
+// Pattern Printing (Right Triangle)
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter number(1-5):";
+    cin>>num;
+    for (int i = 1; i < num+1; i++)
+    {
+        for (int j = 1; j < i+1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+}
