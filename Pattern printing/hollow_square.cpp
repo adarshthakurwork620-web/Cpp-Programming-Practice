3f7c00f7900af54871d422f86815d6a53cde6979
Adarsh22 <adarshthakurwork620@gmail.com>
2026-07-17 19:16:39 +0530

    hollow_square.cpp

---
+++ Pattern printing/hollow_square.cpp
@@ -0,0 +1,28 @@
+// * * * * 
+// *     * 
+// *     * 
+// * * * *
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout<<"Enter the number:";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        //* print
+            for (int j = 1;  j<= n; j++)
+            {
+                
+                if (i==1 or i==n or j==1 or j==n)
+                {
+                   cout<<"* ";
+                }   
+                else
+                    cout<<"  ";
+            }
+           cout<<"\n";       
+    }        
+}
+    
