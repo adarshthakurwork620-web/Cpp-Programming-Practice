76b43f76877ffb9b25bc8a0ab5c63982f3519ae0
Adarsh22 <adarshthakurwork620@gmail.com>
2026-07-20 20:43:29 +0530

    Pyramid.cpp

--- Pattern printing/Pyramid.cpp
+++ Pattern printing/Pyramid.cpp
@@ -3,28 +3,45 @@
 //   *****
 //  *******
 // *********
-
 #include<iostream>
 using namespace std;
 int main()
 {
 int i,j,m,n;
-int s=1;
+// int s=1;
 
 cout<<"Enter the number:" ;
 cin>>n;
 
-for ( i = 0; i < n; i++)
-{
-    for ( j = 0; j < i+1; j++)
+// for ( i = 1; i <= n; i++)
+// {
+//     for ( j = 0; j < n-i; j++)
+//     {
+//         cout<<"-";
+//     }
+//     for ( j = 1; j <= 2*i-1; j++)
+//     {
+//         cout<<"*";
+//     }
+//     cout<<"\n";
+//     // s=s+2;
+// }
+    for (int i = 1; i <= n; i++)
     {
-        cout<<"-";
+        for ( j = 1; j <= n-i; j++)
+        {
+            cout<<"-";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+           cout<<"*";
+        }
+        for (int j = 2; j <=i ; j++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<"\n";  
     }
-    for ( m = s; m > 0; m--)
-    {
-        cout<<"*";
-    }
-    cout<<"\n";
-    s=s+2;
-}
+    
 }
